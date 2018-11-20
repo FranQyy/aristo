5148ce089860c253d0391e5364594fc4e6c2cd23
Lukasz Czerniak <lfczerniak@gmail.com>
2018-11-21 00:23:18 +0100

    custom user models adds

--- accounts/models.py
+++ accounts/models.py
@@ -1,76 +1,46 @@
+from __future__ import unicode_literals
+
 from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, is_staff=False, is_admin=False, is_active=True):
-        if not email:
-            raise ValueError("User must has an email address")
-        user_obj = self.model(
-            email=self.normalize_email(email)
-        )
-        user_obj.is_staff = self.is_staff
-        user_obj.is_admin = self.is_admin
-        user_obj.is_active = self.is_active
-
-        user_obj.set_password(password)
-        user_obj.save(using=self._db)
-        return user_obj
-
-    def create_staffuser(self, email, password=None):
-        user = self.create_user(
-            email,
-            password=password,
-            is_staff=True,
-        )
-        return user
-
-    def create_superuser(self, email, password=None):
-        user = self.create_user(
-            email,
-            password=password,
-            is_staff=True,
-            is_admin=True,
-        )
-        return user
+from .managers import UserManager
 
 
-class User(AbstractBaseUser):
-    email = models.EmailField(unique=True, max_length=255)
-    fullname = models.CharField(max_length=255, blank=True, null=False)
-    active = models.BooleanField(default=True)
-    staff = models.BooleanField(default=False)
-    admin = models.BooleanField(default=False)
-    timestamp = models.DateField(auto_now_add=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = []
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(_('active'), default=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
 
     objects = UserManager()
 
-    def __str__(self):
-        return self.email
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
     def get_full_name(self):
-        return self.email
+        '''
+        Returns the first_name plus the last_name, with a space in between.
+        '''
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
 
     def get_short_name(self):
-        return self.email
+        '''
+        Returns the short name for the user.
+        '''
+        return self.first_name
 
-    @property
-    def is_staff(self):
-        return self.staff
-
-    @property
-    def is_admin(self):
-        return self.admin
-
-    @property
-    def is_active(self):
-        return self.active
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        '''
+        Sends an email to this User.
+        '''
+        send_mail(subject, message, from_email, [self.email], **kwargs)
